f517735ed83064088ad84514a2b4a9901776cbe6
Hannah Wayment-Steele <wayment@aubergine.local>
2024-01-26 11:15:59 -0500

    rm old

--- scripts_hkws_dec2022/run_sap.sh
+++
@@ -1,4 +0,0 @@
-#module load gcc/4.8.5 rosetta
-for x in ../incisive_isoforms/models/omegafold/*.pdb; do
-/n/app/rosetta/3.13/source/bin/rosetta_scripts.default.linuxgccrelease -parser:protocol per_res_sap.xml -beta_nov16 -renumber_pdb -s $x
-done
